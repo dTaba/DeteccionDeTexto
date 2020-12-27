fef8b35591fa9772e406b21118550b2d75545106
dTaba <57154750+dTaba@users.noreply.github.com>
2020-12-27 16:59:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pyt.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tessera
 img = cv.imread('./index.png')
 imgrgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
+## Detectar Letras
+
 hImg, wImg, _ = img.shape
 boxes = pyt.image_to_boxes(imgrgb)
 print(boxes)
@@ -14,8 +16,8 @@ for b in boxes.splitlines():
     b = b.split(' ')
     x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int((b[4]))
     cv.rectangle(imgrgb, (x, hImg - y), (w, hImg- h), (0,0,255), 3)
-    cv.putText(imgrgb, b[0], (x, hImg- y +30), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+    cv.putText(imgrgb, b[0], (x, hImg- y + 25), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
 
-cv.imshow('Lorem Ipsum', imgrgb)
+cv.imshow('Imagen', imgrgb)
 
 cv.waitKey(0)
